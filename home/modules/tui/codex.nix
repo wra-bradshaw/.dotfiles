a7d87b606f059510ae036fb57ecd10e765855a60
will <will.bradshaw50@gmail.com>
2026-06-02 14:06:48 +1000

    fix: remove custom key

--- home/modules/tui/codex.nix
+++ home/modules/tui/codex.nix
@@ -1,17 +1,6 @@
 { pkgs, config, ... }:
 {
   home.packages = [
-    (pkgs.stdenv.mkDerivation {
-      name = "codex";
-      buildInputs = [ pkgs.makeWrapper ];
-      dontUnpack = true;
-
-      installPhase = ''
-        mkdir -p $out/bin
-        makeWrapper ${pkgs.codex}/bin/codex $out/bin/codex --run 'export OPENAI_API_KEY=$(cat ${
-          config.sops.secrets."openai".path
-        })'
-      '';
-    })
+    pkgs.codex
   ];
 }
